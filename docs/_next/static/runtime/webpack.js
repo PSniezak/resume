ff6ddd5c60f863e473ab51deaf2cd36065f166d7
Paul Sniezak <paulsniezak@gmail.com>
2020-07-17 09:19:36 +0200

    Wording updated

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6a2cd0d20a60e893e7e7";
+/******/ 	var hotCurrentHash = "60dddb1236b6afc3877a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
